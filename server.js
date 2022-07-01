d4222d9987ef31c169f1ec60a3907ea2f7f3ce9a
fajlarabbykhan <fajlarabbyk55@gmail.com>
2022-07-01 21:45:14 +0600

    40min

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
 const express = require("express")
 const cors = require('cors')
+const mongoose = require('mongoose');
 const app = express()
 const port = process.env.PORT || 4000;
+
+mongoose.connect('mongodb://localhost/todo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+
+})
+const User = mongoose.model('User', userSchema)
 app.use(cors());
 app.use(express.json());
-app.post("/register", (req, res) => {
-    const {username,password}=req.body
-    res.send({
-        username,
-        password,
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username }).exec();
+    if (user) {
+        res.status(500);
+        res.json({
+            message: "User Already Exist",
+        })
+        return;
+    }
+    // const user= new User({username,password});
+    await User.create({ username, password });
+    res.json({
+        message: "success",
     })
 });
-app.listen(port, () => {
-    console.log(`Example appp listening on port ${port}`);
-})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "))
+db.once("open", function () {
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
+});
